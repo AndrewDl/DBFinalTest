f927c007be564fafe688e5eac00824c64868d451
AndrewDl <dlandrew@ukr.net>
2016-06-14 17:00:15 +0300

    DB managements system with text menu

--- src/DB_Students.java
+++ src/DB_Students.java
@@ -41,14 +41,14 @@ public class DB_Students {
         }
 
     }
-    public static void DB_DELETE(String name){
-        String query = "DELETE FROM Students WHERE name=?";
+    public static void DB_DELETE(int id){
+        String query = "DELETE FROM Students WHERE id=?";
         PreparedStatement statement = null;
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(DB_NAME, DB_USER, DB_PASSWORD);
             statement = connection.prepareStatement(query);
-            statement.setString(1,name);
+            statement.setInt(1,id);
             statement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -108,15 +108,15 @@ public class DB_Students {
         }
 
     }
-    public static void DB_SELECT(String name){
-        String query = "SELECT * FROM Students WHERE name=?";
+    public static void DB_SELECT(double mark){
+        String query = "SELECT * FROM Students WHERE mark=?";
         PreparedStatement statement = null;
         Connection connection = null;
         ResultSet resultSet = null;
         try {
             connection = DriverManager.getConnection(DB_NAME, DB_USER, DB_PASSWORD);
             statement = connection.prepareStatement(query);
-            statement.setString(1,name);
+            statement.setDouble(1, mark);
             resultSet = statement.executeQuery();
             System.out.println("id" + "    " + "name" + "    " + "mark");
             while(resultSet.next()) {
@@ -125,6 +125,72 @@ public class DB_Students {
                 double Mark = resultSet.getDouble(3);
                 System.out.println(id + "    " + Name + "    " + Mark);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if(statement!=null){
+                    statement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(connection!=null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    public static boolean DB_SELECT(String name){
+        boolean flag = false;
+        String query = "SELECT id FROM Students WHERE name=?";
+        PreparedStatement statement = null;
+        Connection connection = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager.getConnection(DB_NAME, DB_USER, DB_PASSWORD);
+            statement = connection.prepareStatement(query);
+            statement.setString(1, name);
+            resultSet = statement.executeQuery();
+            if(resultSet.next()) {
+                flag = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if(statement!=null){
+                    statement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(connection!=null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return flag;
+    }
+    public static void DB_UPDATE(String name, double mark){
+        String query = "UPDATE Students SET mark=? WHERE name=?";
+        PreparedStatement statement = null;
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_NAME, DB_USER, DB_PASSWORD);
+            statement = connection.prepareStatement(query);
+            statement.setString(2,name);
+            statement.setDouble(1,mark);
+            statement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,64 @@
 /**
  * Created by Andrew on 06/10/16.
  */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //DB_Students.DB_INSERT("Dluzhevskyi", 5.0);
-        //DB_Students.DB_INSERT("Don", 5.0);
-        //DB_Students.DB_DELETE("Dluzhevskyi");
-        //DB_Students.DB_SELECT();
-        DB_Students.DB_SELECT("Don");
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.println("1. Новий запис");
+            System.out.println("2. Змінити оцінку");
+            System.out.println("3. Видалити");
+            System.out.println("4. Показати таблицю");
+            System.out.println("5. Показати студентів за оцінкою");
+            System.out.println("6. Вихід");
+            System.out.println("Выберите номер действия, которое вы хотите выполнить:");
+            int n = sc.nextInt();
+            if (n == 1) {
+                sc = new Scanner(System.in);
+                System.out.println("Enter name to insert someone:");
+                String name = sc.nextLine();
+                if(DB_Students.DB_SELECT(name) == false) {
+                    System.out.println("Enter his/her mark:");
+                    double mark = sc.nextDouble();
+                    DB_Students.DB_INSERT(name, mark);
+                    System.out.println("Your insert is successful!");
+                }
+                else{
+                    System.out.println("We have already had this student in the database.");
+                }
+
+            }
+            if (n == 2) {
+                sc = new Scanner(System.in);
+                System.out.println("Enter the name of student:");
+                String name = sc.nextLine();
+                System.out.println("Enter new mark of student:");
+                double mark = sc.nextDouble();
+                DB_Students.DB_UPDATE(name, mark);
+                System.out.println("Your update is successful!");
+            }
+            if (n == 3) {
+                sc = new Scanner(System.in);
+                System.out.println("Enter id to delete someone:");
+                int id = sc.nextInt();
+                DB_Students.DB_DELETE(id);
+                System.out.println("Your delete is successful!");
+            }
+            if (n == 4) {
+                DB_Students.DB_SELECT();
+            }
+            if (n == 5) {
+                sc = new Scanner(System.in);
+                System.out.println("Enter mark of student:");
+                double mark = sc.nextDouble();
+                DB_Students.DB_SELECT(mark);
+            }
+            if (n == 6) {
+                return;
+            }
+
+        }
     }
 }
